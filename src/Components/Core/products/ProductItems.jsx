e43848aaa6522945b402f785298b86ec7108c67e
Krismandanka <krismandanka@gmail.com>
2024-03-24 11:57:13 +0530

    done

--- src/Components/Core/products/ProductItems.jsx
+++ src/Components/Core/products/ProductItems.jsx
@@ -136,6 +136,8 @@ const ProductItems = ({ inProdu }) => {
           navigate('/login');
       }
   }
+  
+
 
   return (
     <>
--- src/Components/Core/products/Slider.jsx
+++ src/Components/Core/products/Slider.jsx
@@ -27,14 +27,14 @@ const Slider = () => {
       >
         <SwiperSlide className="h-5">
           <img
-            src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+            src="https://images.news18.com/ibnlive/uploads/2023/04/world-health-day-2023-wishes-greetings-1-16807701184x3.jpg"
             alt="image 1"
             className="h-full w-full object-cover"
           />
         </SwiperSlide>
         <SwiperSlide>
           <img
-            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+            src="https://media.istockphoto.com/id/1363588189/photo/healthy-lifestyle-on-ketogenic-diet-eating-clean-keto-food-good-health-dietary-in-heart-dish.jpg?s=612x612&w=0&k=20&c=RVW_a2Bq3eYeUWqkUbTUHkiQbGJaAMa9Q2fyljGymgY="
             alt="image 2"
             className="h-full w-full object-cover"
           />
